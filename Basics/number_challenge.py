1e95a7c11408f16b9aa464ca5372a25b40cc9289
Novobura <novobura@gmail.com>
2023-09-29 21:48:07 +0900

    Add files via upload

---
+++ Basics/number_challenge.py
@@ -0,0 +1,5 @@
+num_1 = input("Enter first number: ")
+num_2 = input("Enter second number: ")
+solution = int(num_1) + int(num_2)
+
+print("The sum of {0} and {1} is {2}".format(num_1, num_2, solution))
