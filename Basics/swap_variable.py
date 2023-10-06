fa4665b8b2083aca103324dce4fc66e4866e3d6b
Novissimo <novobura@gmail.com>
2023-10-06 23:36:14 +0900

    Add files via upload

---
+++ Basics/swap_variable.py
@@ -0,0 +1,11 @@
+#swap variables
+
+x = input("enter value of x: ")
+y = input("enter value of y: ")
+
+temp = x
+x = y
+y = temp
+
+print("the value of x after swapping is : {}".format(x))
+print("the value of y after swapping is : {}".format(y))
