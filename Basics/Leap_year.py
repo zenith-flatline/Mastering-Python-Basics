4031257d01347bf20ea921732db7e168268c484c
Novissimo <novobura@gmail.com>
2023-10-12 23:20:21 +0900

    Create Leap_year.py

---
+++ Basics/Leap_year.py
@@ -0,0 +1,14 @@
+#program to check leap year
+
+year = int(input("enter a year:"))
+
+if (year % 400 == 0) and (year % 100 == 0):
+  print("{0} is a leap year ".format(year))
+
+elif (year % 4 == 0) and (year % 100 !=0):
+  print("{0} is a leapyear ".format(year))
+
+else:
+  print("{0} is not a leap year".format(year))
+
+           
