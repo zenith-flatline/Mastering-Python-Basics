1a8504977bfd17db0f7e11f0e79247592b8b5c80
Novissimo <novobura@gmail.com>
2023-10-20 14:30:38 +0900

    Add files via upload

---
+++ Basics/find_largest_number.py
@@ -0,0 +1,16 @@
+#check which number is larger
+
+# take three input as float
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+if (num1 >= num2) and (num1 >= num3):
+    largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+    largest = num2
+else:
+    largest = num3
+
+print('the largest number is ', largest)
