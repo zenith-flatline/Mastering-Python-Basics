3ed6f36fc097c542f5c93910347ec87df0044374
Novissimo <novobura@gmail.com>
2023-10-22 17:45:08 +0900

    Add files via upload

---
+++ Basics/prime_number_checker.py
@@ -0,0 +1,17 @@
+# check is a number is prime
+
+num = int(input("enter a number: "))
+
+if num == 1:
+    print(num, "is not a prime number!")
+elif num > 1:
+    for i in range(2,num):
+        if (num % i) == 0:
+            print(num, " is not a prime number")
+            print(i, "times ", num//i," is ", num)
+            break
+        else:
+            print(num, 'is a prime number')
+            
+else:
+    print(num, " is not a prime number")
