997b8eb30da1af3a95821d6a7d7950b66f4bf7c1
Novissimo <novobura@gmail.com>
2023-11-06 00:21:47 +0900

    Update forgotten_pin_finder.py

--- Basics/forgotten_pin_finder.py
+++ Basics/forgotten_pin_finder.py
@@ -1,8 +1,8 @@
 #python challenge
-def pins_totla(pins_digits):
+def pins_total(pins_digits):
     sum_digits = 0
     for k in pins_digits:
-        sum_digits += pins_digits
+        sum_digits += pins_digits[k]
     return sum_digits
 
     
@@ -10,13 +10,13 @@ def pin_is_ok(pins_digits):
     if pins_digits['fifth'] + pins_digits['third'] == 14 and \
         pins_digits['first'] == pins_digits['second'] * 2 - 1 and \
             pins_digits['fourth'] == pins_digits['second'] + 1 and \
-                pins_digits['second'] + pins_digits['third'] == 10 and \
+                pins_digits['second'] + pins_digits['third'] == 10:
                     if pins_total(pins_digits) == 30:
                         return True
 
 for pins in range(0,100000):
     this_pin = str(pins).zfill(5)
-    print(this_pin)
+
 
     pins_digits = {}
     pins_digits["first"] = int(this_pin[0])
@@ -25,6 +25,6 @@ for pins in range(0,100000):
     pins_digits["fourth"] = int(this_pin[3])
     pins_digits["fifth"] = int(this_pin[4])
 
-    if pin_is_ok(pins_digits:)
+    if pin_is_ok(pins_digits):
         print(pins)
 
